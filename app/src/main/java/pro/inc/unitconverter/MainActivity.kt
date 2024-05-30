242ea78b255a49fce2c3103e1d8f5b5099474495
Protick Saha <85408091+Protick10@users.noreply.github.com>
2024-05-31 00:00:22 +0600

    updated

--- app/src/main/java/pro/inc/unitconverter/MainActivity.kt
+++ app/src/main/java/pro/inc/unitconverter/MainActivity.kt
@@ -4,30 +4,59 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pro.inc.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             UnitConverterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+//                Scaffold(modifier = Modifier.fillMaxSize(),
+//
+//
+//                )
+//                { innerPadding ->
+////                    Greeting(
+////                        name = "Android",
+////                        modifier = Modifier.padding(innerPadding)
+////                    )
+//                    UnitCnverter()
+//                }
+
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    UnitCnverter()
                 }
             }
         }
     }
+}
+
+@Composable
+fun UnitCnverter(){
+    Column {
+            Greeting(name ="Android", modifier = Modifier.padding(16.dp))
+            Greeting(name ="Android", modifier = Modifier.padding(16.dp))
+        Row {
+
+        }
+    }
+
 }
 
 @Composable
